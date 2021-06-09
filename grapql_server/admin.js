32fbcba251ca4704f6cc8dc44f3995a1b46e3d0d
PanzariuAdi <panzariu.adi00@gmail.com>
2021-06-09 11:40:37 +0300

    Admin

--- grapql_server/admin.js
+++ grapql_server/admin.js
@@ -11,10 +11,10 @@ const { buildFederatedSchema} = require('@apollo/federation');
 const { UniqueFieldDefinitionNamesRule } = require('graphql');
 const { argsToArgsConfig } = require('graphql/type/definition');
 const port = 4002;
-const columns = "eventid, iyear, imonth, iday, extended, country_txt, region_txt, provstate, city, latitude, longitude, specificity, vicinity, summary, multiple, success, suicide, attacktype1_txt, targtype1_txt, corp1, target1, natlty1_txt, gname, motive, claimed, weaptype1_txt, weapdetail, nkill, nkillus, nkillter, nwound, ishostkid, addnotes, propextent_txt";
+const columns = "iyear, imonth, iday, extended, country_txt, region_txt, provstate, city, latitude, longitude, specificity, vicinity, summary, multiple, success, suicide, attacktype1_txt, targtype1_txt, corp1, target1, natlty1_txt, gname, motive, claimed, weaptype1_txt, weapdetail, nkill, nkillus, nkillter, nwound, ishostkid, addnotes, propextent_txt";
 const  typeDefs = gql`
         type attack {
-            eventid: Float
+            id: Float
             iyear: Float
             imonth: Float
             iday: Float
@@ -60,13 +60,13 @@ const  typeDefs = gql`
             addArticle(title: String!,date: String, description: String) : String
             updateArticle(id: Float!, title: String, date: String, description: String) : String
             deleteArticle(id: Float!) : String
-            addAttack(eventid: Float!, iyear: Float, imonth: Float, iday: Float, extended: Float, country_txt: String, region_txt: String, provstate: String, city: String, latitude: Float, longitude: Float, specificity: Float, vicinity: Float, summary: String, multiple: Float, success: Float, suicide: Float  , attacktype1_txt: String, targtype1_txt: String, corp: String, target: String, natlty1_txt: String, gname: String, motive: String, claimed: String, weaptype1_txt: String, weapdetail: String, nkill: Float, nkillus: String, nkillter: String, nwounds: Float, ishostkid: Float, addnotes: String, propextent_txt: String) : String
-            updateAttack(eventid: Float!, iyear: Float, imonth: Float, iday: Float, extended: Float, country_txt: String, region_txt: String, provstate: String, city: String, latitude: Float, longitude: Float, specificity: Float, vicinity: Float, summary: String, multiple: Float, success: Float, suicide: Float  , attacktype1_txt: String, targtype1_txt: String, corp: String, target: String, natlty1_txt: String, gname: String, motive: String, claimed: String, weaptype1_txt: String, weapdetail: String, nkill: Float, nkillus: String, nkillter: String, nwounds: Float, ishostkid: Float, addnotes: String, propextent_txt: String) : String
-            deleteAttack(eventid: Float) : String
+            addAttack(id: Float!, iyear: Float, imonth: Float, iday: Float, extended: Float, country_txt: String, region_txt: String, provstate: String, city: String, latitude: Float, longitude: Float, specificity: Float, vicinity: Float, summary: String, multiple: Float, success: Float, suicide: Float  , attacktype1_txt: String, targtype1_txt: String, corp: String, target: String, natlty1_txt: String, gname: String, motive: String, claimed: String, weaptype1_txt: String, weapdetail: String, nkill: Float, nkillus: String, nkillter: String, nwounds: Float, ishostkid: Float, addnotes: String, propextent_txt: String) : String
+            updateAttack(id: Float!, iyear: Float, imonth: Float, iday: Float, extended: Float, country_txt: String, region_txt: String, provstate: String, city: String, latitude: Float, longitude: Float, specificity: Float, vicinity: Float, summary: String, multiple: Float, success: Float, suicide: Float  , attacktype1_txt: String, targtype1_txt: String, corp: String, target: String, natlty1_txt: String, gname: String, motive: String, claimed: String, weaptype1_txt: String, weapdetail: String, nkill: Float, nkillus: String, nkillter: String, nwounds: Float, ishostkid: Float, addnotes: String, propextent_txt: String) : String
+            deleteAttack(id: Float) : String
         }
 
         extend type Query {
-            attacks: [attack]
+            attacks(inf: Float, ext: Float): [attack]
             articles: [article]
             attackById(id: Float!): [attack]
             articleById(id: Int!): [article]
@@ -76,10 +76,10 @@ const  typeDefs = gql`
 
     const resolvers = {
         Query: {
-            attacks: () => {
+            attacks: (_, args) => {
                 return new Promise(function(resolve,reject){
-                    // var columns = "eventid, iyear, imonth, iday, extended, country_txt, region_txt, provstate, city, latitude, longitude, specificity, vicinity, summary, multiple, success, suicide, attacktype1_txt, targtype1_txt, corp1, target1, natlty1_txt, gname, motive, claimed, weaptype1_txt, weapdetail, nkill, nkillus, nkillter, nwound, ishostkid, addnotes, propextent_txt";
-                    connection.query("SELECT " + columns + " FROM terrorism LIMIT 20, 30", function (err, result, attacks) {
+                    // var columns = "iyear, imonth, iday, extended, country_txt, region_txt, provstate, city, latitude, longitude, specificity, vicinity, summary, multiple, success, suicide, attacktype1_txt, targtype1_txt, corp1, target1, natlty1_txt, gname, motive, claimed, weaptype1_txt, weapdetail, nkill, nkillus, nkillter, nwound, ishostkid, addnotes, propextent_txt";
+                    connection.query(`SELECT id, ` + columns + ` FROM terrorism LIMIT ${args.inf}, ${args.ext}`, function (err, result, attacks) {
                         if (err)
                             reject({name:"error: " + err.message});
                         resolve(result);
@@ -89,7 +89,7 @@ const  typeDefs = gql`
 
             attackById: (_, args) => {
                 return new Promise (function(resolve, reject) {
-                    connection.query("SELECT " + columns + " FROM terrorism WHERE eventid = " + args.id, function (err, result, attacks) {
+                    connection.query("SELECT " + columns + " FROM terrorism WHERE id = " + args.id, function (err, result, attacks) {
                         if(err)
                             reject({name:"error: " + err.message});
                         resolve(result);
@@ -146,10 +146,10 @@ const  typeDefs = gql`
                 });
             },
 
-            // var columns = "eventid, iyear, imonth, iday, extended, country_txt, region_txt, provstate, city, latitude, longitude, specificity, vicinity, summary, multiple, success, suicide, attacktype1_txt,
+            // var columns = "iyear, imonth, iday, extended, country_txt, region_txt, provstate, city, latitude, longitude, specificity, vicinity, summary, multiple, success, suicide, attacktype1_txt,
             //  targtype1_txt, corp1, target1, natlty1_txt, gname, motive, claimed, weaptype1_txt, weapdetail, nkill, nkillus, nkillter, nwound, ishostkid, addnotes, propextent_txt";
             addAttack: (_, args) => { 
-                let stmt = `INSERT INTO terrorism (` + columns + `) VALUES (${args.eventid}, ${args.iyear}, ${args.imonth}, ${args.iday}, ${args.extended}, '${args.country_txt}', '${args.region_txt}', '${args.provstate}', '${args.city}', ${args.latitude}, ${args.longitude}, ${args.specificity}, ${args.vicinity}, '${args.summary}', ${args.multiple}, ${args.success}, ${args.suicide}, '${args.attacktype1_txt}','${args.targtype1_txt}','${args.corp}','${args.target}', '${args.natlty1_txt}','${args.gname}','${args.motive}','${args.claimed}','${args.weaptype1_txt}','${args.weapdetail}',${args.nkill},'${args.nkillus}','${args.nkillter}',${args.nwounds},'${args.ishostkid}','${args.addnotes}','${args.propextent_txt}');`
+                let stmt = `INSERT INTO terrorism (` + columns + `) VALUES (${args.iyear}, ${args.imonth}, ${args.iday}, ${args.extended}, '${args.country_txt}', '${args.region_txt}', '${args.provstate}', '${args.city}', ${args.latitude}, ${args.longitude}, ${args.specificity}, ${args.vicinity}, '${args.summary}', ${args.multiple}, ${args.success}, ${args.suicide}, '${args.attacktype1_txt}','${args.targtype1_txt}','${args.corp}','${args.target}', '${args.natlty1_txt}','${args.gname}','${args.motive}','${args.claimed}','${args.weaptype1_txt}','${args.weapdetail}',${args.nkill},'${args.nkillus}','${args.nkillter}',${args.nwounds},'${args.ishostkid}','${args.addnotes}','${args.propextent_txt}');`
 
                 connection.query(stmt, function(err, result) {
                     if(err)
@@ -159,7 +159,7 @@ const  typeDefs = gql`
             },
 
             deleteAttack: (_, args) => {
-                let stmt = `DELETE FROM terrorism WHERE eventid = ${args.eventid}`;
+                let stmt = `D E id = ${args.id}`;
                 connection.query(stmt, function(err, result) {
                     if(err)
                         throw err;
@@ -168,7 +168,7 @@ const  typeDefs = gql`
             },
 
             updateAttack: (_, args) => {
-                let stmt = `UPDATE terrorism SET iyear = ${args.iyear}, imonth = ${args.imonth}, iday = ${args.iday}, extended = ${args.extended}, country_txt = '${args.country_txt}', region_txt = '${args.region_txt}', provstate = '${args.provstate}', city = '${args.city}', latitude = ${args.latitude}, longitude = ${args.longitude}, specificity = ${args.specificity},vicinity =  ${args.vicinity}, summary = '${args.summary}', multiple = ${args.multiple}, success = ${args.success}, suicide = ${args.suicide}, attacktype1_txt = '${args.attacktype1_txt}', targtype1_txt = '${args.targtype1_txt}', corp1 = '${args.corp}' ,target1 = '${args.target}',natlty1_txt = '${args.natlty1_txt}', gname = '${args.gname}', motive = '${args.motive}',claimed = '${args.claimed}', weaptype1_txt = '${args.weaptype1_txt}',weapdetail = '${args.weapdetail}', nkill = ${args.nkill}, nkillus = '${args.nkillus}', nkillter = '${args.nkillter}',nwound = ${args.nwounds}, ishostkid = '${args.ishostkid}', addnotes = '${args.addnotes}',propextent_txt = '${args.propextent_txt}' WHERE eventid = ${args.eventid};`
+                let stmt = `UPDATE terrorism SET iyear = ${args.iyear}, imonth = ${args.imonth}, iday = ${args.iday}, extended = ${args.extended}, country_txt = '${args.country_txt}', region_txt = '${args.region_txt}', provstate = '${args.provstate}', city = '${args.city}', latitude = ${args.latitude}, longitude = ${args.longitude}, specificity = ${args.specificity},vicinity =  ${args.vicinity}, summary = '${args.summary}', multiple = ${args.multiple}, success = ${args.success}, suicide = ${args.suicide}, attacktype1_txt = '${args.attacktype1_txt}', targtype1_txt = '${args.targtype1_txt}', corp1 = '${args.corp}' ,target1 = '${args.target}',natlty1_txt = '${args.natlty1_txt}', gname = '${args.gname}', motive = '${args.motive}',claimed = '${args.claimed}', weaptype1_txt = '${args.weaptype1_txt}',weapdetail = '${args.weapdetail}', nkill = ${args.nkill}, nkillus = '${args.nkillus}', nkillter = '${args.nkillter}',nwound = ${args.nwounds}, ishostkid = '${args.ishostkid}', addnotes = '${args.addnotes}',propextent_txt = '${args.propextent_txt}' WHERE id = ${args.id};`
 
                 connection.query(stmt, function(err, result) {
                     if(err)
